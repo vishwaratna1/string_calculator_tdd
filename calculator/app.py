749a09e3dd338c760e42adbf0bdd4f39638eabc3
Vishwaratna <vishwaratna555@gmail.com>
2025-04-15 19:53:53 +0530

    Added test for single number input | Updated logic to handle single number string

--- calculator/app.py
+++ calculator/app.py
@@ -2,3 +2,5 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
+        
+        return int(numbers)
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,3 +3,6 @@ from tests.fixtures.calculator_fixtures import calculator
 def test_empty_string_returns_zero(calculator):
     assert calculator.add("") == 0
 
+def test_single_number_returns_itself(calculator):
+    assert calculator.add("1") == 1
+
