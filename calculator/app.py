c71bd3d737803d5ead0da4988de41c24a72bd0f9
Vishwaratna <vishwaratna555@gmail.com>
2025-04-16 00:10:36 +0530

    Added test for custom delimiter support | Updated loguc to handle custom delimiter

--- calculator/app.py
+++ calculator/app.py
@@ -2,6 +2,15 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
-        numbers = numbers.replace("\n", ",")
+        delimiters = [",", "\n"]
+        if numbers.startswith("//"):
+            last_delimiter_index = numbers.find("\n")
+            custom_delimiter = numbers[2:last_delimiter_index]
+            delimiters.append(custom_delimiter)
+            numbers = numbers[last_delimiter_index+1: ]
+
+        for delimiter in delimiters:
+            numbers = numbers.replace(delimiter, ",")
+
         result = sum(int(num) for num in numbers.split(","))
         return result
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -12,3 +12,6 @@ def test_multiple_comma_seperated_numbers_returns_sum(calculator):
 def test_multiple_comma_seperated_values_with_new_line_between_numbers(calculator):
     assert calculator.add("1\n2,3") == 6
 
+def test_supports_custom_delimiter(calculator):
+    assert calculator.add("//;\n1;2") == 3
+
